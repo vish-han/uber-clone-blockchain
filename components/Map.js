77a7d20f0ab7c9363137ff7660f724b179178a52
Vishal Chauhan <vshlchauhab@gmail.com>
2022-11-30 14:30:46 +0530

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -6,10 +6,11 @@ const style = {
   wrapper: `flex-1 h-full w-full`,
 }
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+
 
 const Map = () => {
   const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext)
+   mapboxgl.accessToken = 'pk.eyJ1IjoidmlzaGFsZGV2MDkiLCJhIjoiY2xiMmE0czNkMDJlajQxbzZ3M2FnZWphNyJ9.n8w-QvH1go8X6Ak2OwoM_A';
 
   useEffect(() => {
     const map = new mapboxgl.Map({
